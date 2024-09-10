e40b0e2404c5cac4b9ce14965b53ec791b084db7
Lander <landervanmolle1@gmail.com>
2024-09-10 20:02:35 +0200

    Improved QuickStart.bat

--- QuickStart.bat
+++ QuickStart.bat
@@ -12,17 +12,26 @@ if not exist %envFile% (
     echo .env file already exists.
 )
 
-:: Step 2: Ask for user input and write to .env file
-echo Follow the github guide if you dont know where to find the following values: https://github.com/LanderVM/Discord-MusicBot?tab=readme-ov-file#very-quick-setup-windows-only
-set /p DISCORD_BOT_TOKEN="Enter your Discord Bot Token: "
-set /p DISCORD_CLIENT_ID="Enter your Discord Client ID: "
-set /p DISCORD_CLIENT_SECRET="Enter your Discord Client Secret: "
+:: Step 2: Check if the Discord values are already present in the .env file
+findstr /C:"DISCORD_BOT_TOKEN=" %envFile% >nul 2>&1
+if %errorlevel% neq 0 (
+    set /p DISCORD_BOT_TOKEN="Enter your Discord Bot Token: "
+    echo DISCORD_BOT_TOKEN=%DISCORD_BOT_TOKEN% >> %envFile%
+)
 
-echo DISCORD_BOT_TOKEN=%DISCORD_BOT_TOKEN% > %envFile%
-echo DISCORD_CLIENT_ID=%DISCORD_CLIENT_ID% >> %envFile%
-echo DISCORD_CLIENT_SECRET=%DISCORD_CLIENT_SECRET% >> %envFile%
+findstr /C:"DISCORD_CLIENT_ID=" %envFile% >nul 2>&1
+if %errorlevel% neq 0 (
+    set /p DISCORD_CLIENT_ID="Enter your Discord Client ID: "
+    echo DISCORD_CLIENT_ID=%DISCORD_CLIENT_ID% >> %envFile%
+)
 
-:: Step 3: Check if docker is running
+findstr /C:"DISCORD_CLIENT_SECRET=" %envFile% >nul 2>&1
+if %errorlevel% neq 0 (
+    set /p DISCORD_CLIENT_SECRET="Enter your Discord Client Secret: "
+    echo DISCORD_CLIENT_SECRET=%DISCORD_CLIENT_SECRET% >> %envFile%
+)
+
+:: Step 3: Check if Docker Desktop is running
 tasklist /FI "IMAGENAME eq Docker Desktop.exe" 2>NUL | find /I /N "Docker Desktop.exe">NUL
 if "%ERRORLEVEL%"=="1" (
     echo Docker Desktop is not running. Please start Docker Desktop and run this file again.
@@ -31,12 +40,13 @@ if "%ERRORLEVEL%"=="1" (
 )
 
 :: Step 4: Run docker-compose
-
 cd Discord-MusicBot-v4
 docker-compose up -d --build
 
-:: Step 5: Invite
-
-echo Invite the music bot to your discord server using the following url (paste this in a webbrowser): https://discord.com/oauth2/authorize?client_id=%DISCORD_CLIENT_ID%^&scope=bot^&permissions=277083450689
+:: Step 5: Bot invite
+echo //////// BOT INVITE ////////
+echo Invite the music bot to your Discord server using the following URL (paste this in a web browser): 
+echo https://discord.com/oauth2/authorize?client_id=%DISCORD_CLIENT_ID%^&scope=bot^&permissions=277083450689
+echo ///////////////////////////
 
 pause
